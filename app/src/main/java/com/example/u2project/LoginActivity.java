ba6ff006ac9ca426df00ddebedccead3c62f408d
skyler <april260@naver.com>
2023-12-11 18:38:41 +0900

    ver 0.0.1

--- app/src/main/java/com/example/u2project/LoginActivity.java
+++ app/src/main/java/com/example/u2project/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.u2project;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -18,7 +19,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
-
     private FirebaseAuth mFirebaseAuth;     // 파이어베이스 인증
     private DatabaseReference mDatabaseRef; // 실시간 데이터베이스
     private EditText mEtEmail, mEtPwd;      // 회원가입 입력 필드
@@ -34,7 +34,7 @@ public class LoginActivity extends AppCompatActivity {
         mEtEmail = findViewById(R.id.et_email);
         mEtPwd = findViewById(R.id.et_pwd);
 
-        ImageView btn_login = findViewById(R.id.btn_login);
+        Button btn_login = findViewById(R.id.btn_login);
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -61,7 +61,7 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         // 회원가입 버튼을 눌렀을때
-        ImageView btn_register = findViewById(R.id.btn_register);
+        Button btn_register = findViewById(R.id.btn_register);
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
